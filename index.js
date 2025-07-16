75ca9a545008693acc7e30e0a4b284986e776a34
zgoodman27 <zgoodman27@gmail.com>
2025-07-15 23:41:33 -0400

    finalized project

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 app.use(express.static("public"));
+app.use(cookieParser());
 
 const PORT = 3000;
+
+
+app.get("/lightMode", (req, res) => {
+  res.cookie("theme", "lightMode", { maxAge: 60 * 60 * 24});
+  res.redirect("/");
+});
+
+app.get("/darkMode", (req, res) => {
+  res.cookie("theme", "darkMode", { maxAge: 60 * 60 * 24});
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
--- public/index.js
+++ public/index.js
@@ -1,1 +1,71 @@
 // Your code goes here
+
+//set up event listeners
+const darkModeButton = document.getElementById("darkMode");
+const lightModeButton = document.getElementById("lightMode");
+const bodyElement = document.body;
+
+// Function to apply the theme to the body
+function applyTheme(theme) {
+  bodyElement.className = theme;
+}
+
+//function to load the theme from the cookie
+function themeFromCookie() {
+  const cookies = document.cookie;
+  console.log("Current cookies:", cookies); //log any cookies so we know if we need to add chocolate chips or raisins...
+
+  const themeCookie = cookies
+    .split("; ")
+    .find((row) => row.startsWith("theme="));
+
+  if (themeCookie) {
+    const theme = themeCookie.split("=")[1];
+    if (theme === "darkMode" || theme === "lightMode") {
+      applyTheme(theme);
+      console.log(`applying theme from cookie :${theme}`);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", themeFromCookie);
+
+//set up event listener for light mode
+lightModeButton.addEventListener("click", () => {
+  bodyElement.className = "lightMode";
+
+  //send the get request to switch to light mode
+  fetch("/lightMode")
+    .then((response) => {
+      if (!response.ok) {
+        console.error("Failure to load Light Mode");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      console.log("Server response for Light Mode", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching Light Mode", error);
+    });
+});
+
+//set up event listener for dark mode
+darkModeButton.addEventListener("click", () => {
+  bodyElement.className = "darkMode";
+
+  //send the get request to switch to dark mode
+  fetch("/darkMode")
+    .then((response) => {
+      if (!response.ok) {
+        console.error("Failure to load Dark Mode");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      console.log("Server response for Dark Mode", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching Dark mode", error);
+    });
+});
